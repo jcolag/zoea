42a56d2e5b6578bf29c164c495e530ff0973cb5c
John Colagioia <john@colagioia.net>
2019-02-15 06:43:12 -0500

    Oversimplified single-message component

---
+++ message.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { render, Area, Form, StyledText, Text } from 'proton-native';
+
+export default class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: this.props.markdown,
+    }
+  }
+
+  render() {
+    return (
+        <StyledText
+          style={{
+            fontSize: 20,
+            fontWeight: 'normal',
+          }}
+        >
+          {this.state.message}
+        </StyledText>
+    );
+  }
+}
+
