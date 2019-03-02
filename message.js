32fba4f95178d5fd12f9a81c90d4cc722f1af04c
John Colagioia <john@colagioia.net>
2019-03-02 06:49:11 -0500

    Add unique key to message components

--- message.js
+++ message.js
@@ -20,6 +20,13 @@ export default class Message extends Component {
       username: this.props.user,
     }
   }
+  
+  uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
   textWidth(text) {
     const quarter = 'Iijl`|;\':,.';
@@ -86,6 +93,7 @@ export default class Message extends Component {
           // Reset
           messages.push(<Markdown
             text={sub}
+            key={this.uuidv4()}
           />);
           sub = '';
           sublen = 0;
