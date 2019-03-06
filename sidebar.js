88948cd369833971052b20e4c458b14d29141e2c
John Colagioia <john@colagioia.net>
2019-03-06 06:47:12 -0500

    Add a very rough do-nothing sidebar component

---
+++ sidebar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+  Box,
+  Text,
+  render,
+} from 'proton-native';
+
+export default class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      column: props.column,
+      items: props.items,
+      row: props.row,
+    }
+  }
+
+  render() {
+    let itemCount = 0;
+    const list = [];
+
+    this.state.items.forEach(item =>
+      list.push(
+        <Text
+          key={itemCount++}
+        >
+          {item}
+        </Text>
+      )
+    );
+    return (
+      <Box row={this.state.row} column={this.state.column} stretchy={false}>
+        {list}
+      </Box>
+    );
+  }
+}
+
