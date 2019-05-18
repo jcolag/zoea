d2c7485ae60859e5bbb28512604b4ed2a2ed6f50
John Colagioia <john@colagioia.net>
2019-05-18 07:47:26 -0400

    Move old Scuttlebutt code out of the way

--- zoea.js
+++ zoea.js
@@ -6,66 +6,17 @@ import MessagePanel from './messagepanel';
 import { alert } from './dialog';
 import ErrorBoundary from './errorboundary';
 
-const ssbClient = require('ssb-client');
-const pull = require('pull-stream');
+const spawn = require('threads').spawn;
 
 class MainWindow extends Component {
   constructor(props) {
     super(props);
-    const timeoutId = setTimeout(this.stubData, 500, this);
     this.state = {
       endIdx: 4,
       messages: [],
       sideElements: [],
       startIdx: 0,
-      timeoutId,
     }
-  }
-  
-  stubData(who) {
-    const messages = [];
-    const sideElements = [
-      "Sidebar",
-    ];
-    
-    clearTimeout(who.state.timeoutId);
-    ssbClient(function (err, sbot) {
-      let scutId;
-      if (err) {
-        alert(err, "SSB Client Error");
-        throw err;
-      }
-
-      sbot.whoami(function(err, info) {
-        const stream = sbot.createUserStream({
-          id: info.id,
-        });
-        pull (stream, pull.collect((err, msgs) => {
-          if (err) {
-            alert(err, "SSB Client Error");
-            sbot.close();
-            throw err;
-          }
-          msgs.forEach(m => {
-            if (m.value.content.type !== 'post') {
-              return;
-            }
-            messages.push({
-              author: m.value.author,
-              contents: m.value.content.text,
-              ts: new Date(m.value.timestamp),
-            });
-          });
-          messages.reverse();
-          who.setState({
-            messages,
-            sideElements,
-            timeoutId: null,
-          });
-          sbot.close();
-        }));
-      });
-    });
   }
   
   render() {
